3a64920868b6f996037a22b47c0e9b3f18021f8e
beavinash <beavinash15@gmail.com>
2016-11-17 08:05:28 +0000

    styling done for show page

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var express     = require("express"),
 mongoose.connect("mongodb://localhost/eine_practice");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"))
+console.log(__dirname)
 seedDB();
     
 app.get("/", function(req, res){
